7a0264964d8ae03844fe732d2a488de8db6564ff
QING ZHOU <qczhou@ucdavis.edu>
2016-08-22 11:39:56 -0700

    functions in DBO for update and delete entries in database

--- classes/DBO.class.php
+++ classes/DBO.class.php
@@ -34,5 +34,29 @@ class DBO {
             $this->data[$name] = $value;
         }
     }
+    
+    function update(array $updated_columns) {
+        $pairs = array();
+        foreach($updated_columns as $key => $value ) {
+            array_push($pairs, $key . '=' . $this->db->escape_string($value));
+        } 
+        $sql = sprintf(
+                'UPDATE %s SET %s WHERE %s = %s',
+                $this->table_name,
+                $this->implode(',',$pairs),
+                $this->primary_key,
+                $this->data[$this->primary_key]
+                );
+        $this->db->query($sql);
+    }
+    
+    function delete() {
+        $sql = sprintf('DELETE * FROM %s WHERE %s = %s',
+                $this->table_name,
+                $this->primary_key,
+                $this->data[$this->primary_key]
+                );
+        $this->db->query($sql);
+    }
 }
 ?>
