d1fe6909ba5517d5f93b26731fa7a33f6028fd1f
QING ZHOU <qczhou@ucdavis.edu>
2016-08-23 12:12:04 -0700

    check out page

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $customer_id = 1;
 
 
 <!-- ko if: data.orders_array().length > 0 -->
-<div class="cart-main">
+<div class="cart-main col-md-12 col-sm-12">
     <div class="catalog col-md-12 col-sm-12" id="cart">
         <h3>Here's What You're Getting!</h3>
         <hr>
@@ -74,6 +74,149 @@ $customer_id = 1;
 </div>
 <!-- /ko -->
 
+<div class="checkout-main col-md-12 col-sm-12" id="billing">
+    
+        <div class="col-md-6 col-sm-6">
+            <div class="review-order">
+                <h3>Review Your Order</h3>
+                <hr>
+                <div class="block col-md-12 col-md-12">
+                    <table style="width:100%;">
+                        <tr>
+                            <th style="width:30%;">Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Subtotal</th>
+                        </tr>
+                        <!-- ko foreach:data.orders_array -->
+                        <tr>
+                            <td><span data-bind="html:$parent.get_product_by_id($data.product_id()).product_name()"></span></td>
+                            <td><span data-bind="html:$data.quantity"></span></td>
+                            <td>$<span data-bind="html:$parent.get_product_by_id($data.product_id()).product_price()"></span></td>
+                            <td>$<span data-bind="html:$data.total"></span></td>
+                        </tr>
+                        <!-- /ko -->
+                        <tr>
+                            <td colspan="2"></td>
+                            <td><b>Total</b></td>
+                            <td><b>$<span data-bind="html:data.subtotal"></span></b></td>
+                        </tr>
+                    </table>
+
+                </div>
+
+
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6">
+            <div class="address">
+                <h3>Shipping Address</h3>
+                <hr>
+                <div class="block col-md-6 col-md-6">
+                    <label>First Name</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>Last Name</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-sm-12">
+                    <label>Email Address</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-sm-12">
+                    <label>Address 1</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-md-12">
+                    <label>Address 2</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>State</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>Postal Code</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-md-12">
+                    <input type="checkbox">
+                    <label>Use this address for billing</label>
+                </div>
+            </div>
+        </div>   
+</div>
+<div class="checkout-main col-md-12 col-sm-12" id="shipping">
+        <div class="col-md-6 col-sm-6">
+            <div class="payment-info">
+                <h3>Payment Information</h3>
+                <hr>
+                <div class="block col-md-6 col-md-6">
+                    <label>First Name</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>Last Name</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-sm-12">
+                    <label>Card Number</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-4 col-sm-4">
+                    <label>Month</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-4 col-md-4">
+                    <label>Year</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-4 col-md-4">
+                    <label>CVV</label>
+                    <input class="form-control">
+                </div>
+            </div>
+        </div> 
+        <div class="col-md-6 col-sm-6">
+            <div class="address">
+                <h3>Billing Address</h3>
+                <hr>
+                <div class="block col-md-6 col-md-6">
+                    <label>First Name</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>Last Name</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-sm-12">
+                    <label>Email Address</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-sm-12">
+                    <label>Address 1</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-md-12">
+                    <label>Address 2</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>State</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-6 col-md-6">
+                    <label>Postal Code</label>
+                    <input class="form-control">
+                </div>
+                <div class="block col-md-12 col-md-12">
+                    <input type="checkbox">
+                    <label>Use this address for billing</label>
+                </div>
+            </div>
+        </div> 
+</div>
 
 
 
