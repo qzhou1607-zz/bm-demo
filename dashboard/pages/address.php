3afda35250b3cef6c52bcb37f8e8c3d334b58974
QING ZHOU <qczhou@ucdavis.edu>
2016-09-14 11:46:27 -0700

    code review

--- dashboard/pages/address.php
+++ dashboard/pages/address.php
@@ -48,7 +48,7 @@ if(sizeof($_REQUEST) > 0) {
                             <a href="#"><i class="fa fa-wrench fa-fw"></i> Settings <span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="address.php">Address</a>
+                                    <a href="address.php">Shipping Preference</a>
                                 </li>
 <!--                                <li>
                                     <a href="payment.php">Payment</a>
@@ -67,7 +67,7 @@ if(sizeof($_REQUEST) > 0) {
            
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Address</h1>
+                    <h1 class="page-header">Shipping Preference</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -77,7 +77,7 @@ if(sizeof($_REQUEST) > 0) {
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Please Fill in Your Mailing Address Information 
+                            Please Fill in Your Mailing Address and Unit Information 
                         </div>
                         <div class="panel-body">
                             <div class="row">
